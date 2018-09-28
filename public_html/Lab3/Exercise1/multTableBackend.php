d1029765313bffb5b0d969d673d109040b5c5eee
Mitch Simmons <mitch.t.simmons@ku.edu>
2018-09-28 11:58:31 -0500

    first commit

---
+++ public_html/Lab3/Exercise1/multTableBackend.php
@@ -0,0 +1,17 @@
+<?php
+
+function multiply($column, $row) {
+     $answer = $column * $row;
+     return $answer;
+}
+
+for($i = 0; $i <= 100; $i++) {
+     for($j = 0; $j <= 100; $j++) {
+          echo "there is a value";
+     }
+}
+
+
+
+
+ ?>
